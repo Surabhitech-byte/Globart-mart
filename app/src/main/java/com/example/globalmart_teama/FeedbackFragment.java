78657c36fc9a5fa41675ce8a1a3d8b7e0c875849
surabhi chaudhary <sr739306@dal.ca>
2019-07-23 09:39:27 -0300

    feedbackPage created

--- app/src/main/java/com/example/globalmart_teama/FeedbackFragment.java
+++ app/src/main/java/com/example/globalmart_teama/FeedbackFragment.java
@@ -1,20 +1,33 @@
 package com.example.globalmart_teama;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FeedbackFragment extends Fragment {
-
-
+    PopupWindow popupWindow;
+    private Activity mActivity;
     public FeedbackFragment() {
+
+
         // Required empty public constructor
     }
 
@@ -23,7 +36,65 @@ public class FeedbackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feedback, container, false);
+        View view = inflater.inflate(R.layout.fragment_feedback, container, false);
+
+        final Button feedbackBtn  = view.findViewById(R.id.submit_feedback);
+        final EditText feedbackTxt = view.findViewById(R.id.feedback_text);
+        final TextView t = view.findViewById(R.id.feedback_text_id);
+        final FrameLayout linearLayout = (FrameLayout) view.findViewById(R.id.frmLayout);
+
+        feedbackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                feedbackTxt.setVisibility(View.INVISIBLE);
+                feedbackBtn.setVisibility(View.INVISIBLE);
+                t.setVisibility(View.GONE);
+
+                LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                View popUpView = layoutInflater.from(getActivity()).inflate(R.layout.popup_feedback_submit, null);
+
+
+                Button closePopupBtn = (Button) popUpView.findViewById(R.id.closePopupBtn);
+                popupWindow = new PopupWindow(popUpView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                popupWindow.showAtLocation(linearLayout, Gravity.CENTER, 0, 0);
+
+                closePopupBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+
+                        popupWindow.dismiss();
+
+                        final FragmentManager fm = getFragmentManager();
+                        final FragmentTransaction ft = fm.beginTransaction();
+                        HomeFragment h = new HomeFragment();
+                        ft.replace(((ViewGroup) (getView().getParent())).getId()
+                                ,h, "Home");
+                        ft.addToBackStack(null);
+                        ft.commit();
+
+
+                    }
+                });
+
+
+
+            }
+        });
+
+
+
+
+
+
+        return view;
+
+
     }
 
 }
